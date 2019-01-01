dcc6163786fd92936a66b519717fb2827cbc2d35
Iyalla Alamina (Researcher) <u1273400@sb247m-16.local>
2019-01-01 15:37:25 +0000

    kibooks restart

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
-namespace BlazorApp1
+namespace kibooks
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BlazorApp1
+namespace kibooks
 {
     public class Startup
     {
